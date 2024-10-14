31ff71be4a43bfd7b70b336ff2d49d84bda0e465
nh19941995 <96659389+nh19941995@users.noreply.github.com>
2024-10-14 16:29:14 +0700

    backend

---
+++ healthcare_management/src/main/java/org/example/healthcare_management/HealthcareManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.healthcare_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareManagementApplication.class, args);
+    }
+
+}

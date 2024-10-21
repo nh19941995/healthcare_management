f13aa5c03ac27a1cb8cf999d59b3e768ec52320a
nh19941995 <96659389+nh19941995@users.noreply.github.com>
2024-10-21 16:39:43 +0700

    - thêm validate user

--- healthcare_management/src/main/java/org/example/healthcare_management/entities/User.java
+++ healthcare_management/src/main/java/org/example/healthcare_management/entities/User.java
@@ -1,6 +1,7 @@
 package org.example.healthcare_management.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.example.healthcare_management.enums.Gender;
 import org.example.healthcare_management.enums.Status;
@@ -22,31 +23,49 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name must be less than 100 characters")
     @Column(name = "name")
     private String name;
 
-    @Column(name = "username")
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Column(name = "username", unique = true)
     private String username;
 
-    @Column(name = "email")
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Column(name = "email", unique = true)
     private String email;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern.List({
+            @Pattern(regexp = ".*[A-Z].*", message = "Password must contain at least one uppercase letter"),
+            @Pattern(regexp = ".*[a-z].*", message = "Password must contain at least one lowercase letter"),
+            @Pattern(regexp = ".*[0-9].*", message = "Password must contain at least one number"),
+            @Pattern(regexp = ".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*", message = "Password must contain at least one special character")
+    })
     @Column(name = "password")
     private String password;
 
+    @Size(max = 255, message = "Address must be less than 255 characters")
     @Column(name = "address")
     private String address;
 
+    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Phone number should be valid")
     @Column(name = "phone")
     private String phone;
 
     @Column(name = "avatar")
     private String avatar;
 
+    @NotNull(message = "Gender is required")
     @Column(name = "gender")
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @Size(max = 255, message = "Description must be less than 255 characters")
     @Column(name = "description", columnDefinition = "TEXT")
     private String description;
 
